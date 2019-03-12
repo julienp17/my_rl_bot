03ae440551873c60fd58b9ce695a02840d5fd9af
julienp17 <julien.pause17@gmail.com>
2019-03-12 17:54:35 +0400

    Follow Part 2 guide
    https://www.reddit.com/r/RocketLeagueBots/comments/6vys9y/how_to_create_a_rocket_league_bot_part_2_aiming/

--- my_bot/my_bot.py
+++ my_bot/my_bot.py
@@ -1,81 +1,50 @@
-import math
+# -*- coding: utf-8 -*-
 
+import math
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
+class RoucoolBot(BaseAgent):
 
-class PythonExample(BaseAgent):
+    def __init__(self, name, team, index):
 
-    def initialize_agent(self):
-        #This runs once before the bot starts up
-        self.controller_state = SimpleControllerState()
+        # Run BaseAgent's init
+        super().__init__(name, team, index)
+        self.controller = SimpleControllerState()
+
+        # Game data
+        self.bot_pos = None
+        self.bot_rot = None
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
-        ball_location = Vector2(packet.game_ball.physics.location.x, packet.game_ball.physics.location.y)
 
-        my_car = packet.game_cars[self.index]
-        car_location = Vector2(my_car.physics.location.x, my_car.physics.location.y)
-        car_direction = get_car_facing_vector(my_car)
-        car_to_ball = ball_location - car_location
+        # Update bot current position & rotation
+        self.bot_yaw = packet.game_cars[self.team].physics.rotation.yaw
+        self.bot_pos = packet.game_cars[self.index].physics.location
+        
+        # Get ball position & aim at it
+        ball_pos = packet.game_ball.physics.location
+        self.aim(ball_pos.x, ball_pos.y)
 
-        steer_correction_radians = car_direction.correction_to(car_to_ball)
+        # Drive forward
+        self.controller.throttle = 1
 
-        if steer_correction_radians > 0:
-            # Positive radians in the unit circle is a turn to the left.
-            turn = -1.0  # Negative value for a turn to the left.
-            action_display = "turn left"
+        return self.controller
+
+    def aim(self, target_x, target_y):
+        angle_between_bot_and_target = math.atan2(target_y - self.bot_pos.y,
+                                                  target_x - self.bot_pos.x)
+        angle_front_to_target = angle_between_bot_and_target - self.bot_yaw
+
+        # Correct the values
+        if angle_front_to_target < -math.pi:
+            angle_front_to_target += 2 * math.pi
+        if angle_front_to_target > math.pi:
+            angle_front_to_target -= 2 * math.pi
+
+        if angle_front_to_target < math.radians(-5):
+            self.controller.steer = -1
+        elif angle_front_to_target > math.radians(10):
+            self.controller.steer = 1
         else:
-            turn = 1.0
-            action_display = "turn right"
-
-        self.controller_state.throttle = 1.0
-        self.controller_state.steer = turn
-
-        draw_debug(self.renderer, my_car, packet.game_ball, action_display)
-
-        return self.controller_state
-
-class Vector2:
-    def __init__(self, x=0, y=0):
-        self.x = float(x)
-        self.y = float(y)
-
-    def __add__(self, val):
-        return Vector2(self.x + val.x, self.y + val.y)
-
-    def __sub__(self, val):
-        return Vector2(self.x - val.x, self.y - val.y)
-
-    def correction_to(self, ideal):
-        # The in-game axes are left handed, so use -x
-        current_in_radians = math.atan2(self.y, -self.x)
-        ideal_in_radians = math.atan2(ideal.y, -ideal.x)
-
-        correction = ideal_in_radians - current_in_radians
-
-        # Make sure we go the 'short way'
-        if abs(correction) > math.pi:
-            if correction < 0:
-                correction += 2 * math.pi
-            else:
-                correction -= 2 * math.pi
-
-        return correction
-
-
-def get_car_facing_vector(car):
-    pitch = float(car.physics.rotation.pitch)
-    yaw = float(car.physics.rotation.yaw)
-
-    facing_x = math.cos(pitch) * math.cos(yaw)
-    facing_y = math.cos(pitch) * math.sin(yaw)
-
-    return Vector2(facing_x, facing_y)
-
-def draw_debug(renderer, car, ball, action_display):
-    renderer.begin_rendering()
-    # draw a line from the car to the ball
-    renderer.draw_line_3d(car.physics.location, ball.physics.location, renderer.white())
-    # print the action that the bot is taking
-    renderer.draw_string_3d(car.physics.location, 2, 2, action_display, renderer.white())
-    renderer.end_rendering()
+            self.controller.steer = 0
